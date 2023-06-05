dd1bdf18b7b43488644b7289ccad6ea88da7fde1
Ilya Kupriyanov <ikuprianov423@gmail.com>
2023-06-05 16:00:31 +0300

    Deleted  used files

--- main/include/error.h
+++
@@ -1,8 +0,0 @@
-/* This header file is intended for local error in printing system */
-
-//destination error
-typedef enum printer_error_e
-{
-    ERROR_DISCONNECT_DEST,          //Disconnected printer error
-    OTHER_ERROR                     //Other types error
-} printer_error;
